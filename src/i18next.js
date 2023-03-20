639be81f955d589e898101f9b4847801abb567ad
Backwards <deema.light@gmail.com>
2023-03-20 23:22:24 +0300

    refactor: change build settings

--- src/i18next.js
+++ src/i18next.js
@@ -1,23 +1,23 @@
-import i18n from 'i18next'
-import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import { initReactI18next } from 'react-i18next'
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
-  .use (initReactI18next)
+  .use(initReactI18next)
   .init({
-    fallbackLng: 'ru',
+    fallbackLng: "ru",
     whitelist: ["ru", "en"],
     debug: false,
     detection: {
-      order: ['localStorage', 'cookie'],
-      caches: ['localStorage', 'cookie']
+      order: ["localStorage", "cookie"],
+      caches: ["localStorage", "cookie"],
     },
     interpolation: {
-      escapeValue: false
-    }
-  })
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
